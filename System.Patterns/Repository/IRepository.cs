f26b94561e6bfdcfda7050429ecf3e644cf1eb43
ermac0 <patrickpulka@gmail.com>
2017-01-02 14:39:56 +0000

    Add project files.

---
+++ System.Patterns/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace System.Patterns.Repository
+{
+    public interface IRepository<TEntity, TKey>
+    {
+        IQueryable<TEntity> GetAll();
+
+        TEntity Get(TKey id);
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        void Add(TEntity entity);
+    }
+
+    public interface IRepository<TEntity> : IRepository<TEntity, int>
+    {
+    }
+}
